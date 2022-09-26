d94d0341410c4311904b6a09ea7380801ce0a8f7
Amanda Briones <amandabriones111@gmail.com>
2022-09-26 16:38:13 -0700

    commit tree a2f2d02a9f4b8ca57d897407d16ebf9185a332e9

--- main.js
+++ main.js
@@ -3,8 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector("#modal")
+errorModal.classList.add("hidden")
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM CONTENT HAS BEEN LOADED");
+  errorModal.classList.add("hidden");
+  clickListener();
+});
 
+function hideError() {
+  errorModal.classList.add("hidden");
+}
+
+function clickListener() {
+  document.addEventListener("click", (event) => {
+    if(event.target.classList[0] === 'like-glyph') {
+      //PROMISE! ASYNC, WE NEED A .THEN
+
+      mimicServerCall()
+        .then((resp) => {
+          const activated = event.target.classList.contains("activated-heart");
+          if(activated) {
+            event.target.classList.remove("activated-heart");
+            event.target.innerHTML = EMPTY_HEART;
+          }else {
+            event.target.classList.add("activated-heart");
+            event.target.innerHTML = FULL_HEART;
+          }
+          activated;
+        })//.300 secs
+        .catch((error) => {
+          console.log(error);
+          errorModal.remove("hidden");
+          setTimeout(() => {
+            hideError();
+          }, 3000);
+        });//PROMISE FAILS, .CATCH -> CATCHES IT
+    }
+  });
+}
 
 
 //------------------------------------------------------------------------------
